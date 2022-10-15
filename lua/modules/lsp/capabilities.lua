8d88fa0521c20ec71c9b6816d66754b96f951058
bryant-the-coder <foobryant99@gmail.com>
2022-10-15 22:49:05 +0800

    fix warning

--- lua/modules/lsp/capabilities.lua
+++ lua/modules/lsp/capabilities.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local completion = capabilities.textDocument.completion.completionItem
 capabilities.textDocument.completion.completionItem.preselectSupport = true
 capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
