6ee9d0b3c0a474aa58ad2454cd0d39e789ba4f28
bryant-the-coder <foobryant99@gmail.com>
2022-11-20 05:57:15 +0800

    lightspeed upgrade

--- lua/modules/tools/lightspeed.lua
+++ lua/modules/tools/lightspeed.lua
@@ -6,3 +6,24 @@ local default_keymaps = {
 for _, m in ipairs(default_keymaps) do
     vim.keymap.set(m[1], m[2], m[3], { noremap = true, silent = true })
 end
+
+require("lightspeed").setup {
+    ignore_case = false,
+    exit_after_idle_msecs = { unlabeled = 1000, labeled = nil },
+    --- s/x ---
+    jump_to_unique_chars = { safety_timeout = 400 },
+    match_only_the_start_of_same_char_seqs = true,
+    force_beacons_into_match_width = true,
+    -- Display characters in a custom way in the highlighted matches.
+    substitute_chars = { ["\r"] = "¬" },
+    -- Leaving the appropriate list empty effectively disables "smart" mode,
+    -- and forces auto-jump to be on or off.
+    -- These keys are captured directly by the plugin at runtime.
+    special_keys = {
+        next_match_group = "<space>",
+        prev_match_group = "<tab>",
+    },
+    --- f/t ---
+    limit_ft_matches = nil,
+    repeat_ft_with_target_char = false,
+}
