f2499f08a252cd6b3379cb6be064430f402b0ee4
bryant-the-coder <foobryant99@gmail.com>
2022-11-24 20:14:56 +0800

    signature upgrade

--- lua/modules/lsp/signature.lua
+++ lua/modules/lsp/signature.lua
@@ -1,19 +1,21 @@
-local cfg = {
+local border = require("custom.border").styles.type_1
+local opts = {
     bind = true,
     doc_lines = 0,
     floating_window = true,
     fix_pos = true,
     hint_enable = true,
-    hint_prefix = " ",
+    hint_prefix = "🐺 ",
     hint_scheme = "String",
     hi_parameter = "Search",
     max_height = 22,
     max_width = 120, -- max_width of signature floating_window, line will be wrapped if exceed max_width
     handler_opts = {
-        border = "single", -- double, single, shadow, none
+        border = border, -- double, single, shadow, none
     },
     zindex = 200, -- by default it will be on top of all floating windows, set to 50 send it to bottom
     padding = "", -- character to pad on left and right of signature can be ' ', or '|'  etc
+    always_trigger = true,
 }
 
-require("lsp_signature").setup(cfg)
+require("lsp_signature").setup(opts)
