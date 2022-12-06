87a49b98137857a0905a1a7e78d2972e0e1ba193
bryant-the-coder <foobryant99@gmail.com>
2022-12-07 05:37:04 +0800

    remove useless comments

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -66,12 +66,6 @@ o.smartcase = true -- Don't ignore when uppercase search
 
 -- Statusline
 o.laststatus = 3
---[[ if utils.has_version "0.8" then
-    o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
-end ]]
--- o.statusline = "%!v:lua.require'custom.statusline'.run()"
--- o.guifont = "OperatorMonoSSmLig Nerd Font,codicon:h10"
--- o.guifont = "ComicCodeLigatures Nerd Font:codicon:h09"
 o.guifont = "Operator Mono SSm Lig Book:h9"
 
 -- Conceal
@@ -90,7 +84,7 @@ o.fillchars = {
 }
 o.listchars = {
     trail = "·",
-    -- eol = "↲",
+    eol = "↲",
 }
 
 -- Backups / Undo
@@ -105,18 +99,3 @@ vim.g.c_syntax_for_h = true
 
 -- Enable bundled tree-sitter parser for Lua
 vim.g.ts_highlight_lua = true
-
--- Terminal
---[[ if vim.fn.has "win32" == 1 then
-    o.shell = "pwsh.exe -nol"
-    o.shellcmdflag = "-nop -c"
-    o.shellquote = '"'
-    o.shellxquote = ""
-    o.shellpipe = "| Out-File -Encoding UTF8 %s"
-    o.shellredir = "| Out-File -Encoding UTF8 %s"
-end ]]
-
--- Using nu as a shell
--- Commenting this out because it causes error on linux cuz i don't hv nu LMFAO
---[[ vim.opt.shell = "nu"
-vim.opt.shellcmdflag = "-c" ]]
