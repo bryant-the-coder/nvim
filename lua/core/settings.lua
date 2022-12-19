157b633366053b5c36f99089de5a93128f1f1f69
bryant-the-coder <foobryant99@gmail.com>
2022-12-19 09:59:20 +0800

    fix smth

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -95,7 +95,7 @@ o.swapfile = false -- Don't write swapfile ]]
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = os.getenv "HOME" .. "/.vim/undodir"
 vim.opt.undofile = true
 
 -- Syntax
