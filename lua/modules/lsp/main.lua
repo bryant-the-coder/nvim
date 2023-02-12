7c60ca3fc224b8edd411d349c9a9533e2ce37883
bryant-the-coder <foobryant99@gmail.com>
2023-02-12 10:04:13 +0800

    remove some stuff from lsp

--- lua/modules/lsp/main.lua
+++ lua/modules/lsp/main.lua
@@ -42,29 +42,6 @@ local sumneko = {
                     "--log-level=trace",
                 },
             },
-            diagnostics = {
-                globals = { "vim" },
-                -- enable = false,
-                groupSeverity = {
-                    strong = "Warning",
-                    strict = "Warning",
-                },
-                groupFileStatus = {
-                    ["ambiguity"] = "Opened",
-                    ["await"] = "Opened",
-                    ["codestyle"] = "None",
-                    ["duplicate"] = "Opened",
-                    ["global"] = "Opened",
-                    ["luadoc"] = "Opened",
-                    ["redefined"] = "Opened",
-                    ["strict"] = "Opened",
-                    ["strong"] = "Opened",
-                    ["type-check"] = "Opened",
-                    ["unbalanced"] = "Opened",
-                    ["unused"] = "Opened",
-                },
-                unusedLocalExclude = { "_*" },
-            },
             format = {
                 enable = false,
                 defaultConfig = {
