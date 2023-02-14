8d537368b5e4e5fd70140d17acd7375a5db21e1f
bryant-the-coder <foobryant99@gmail.com>
2023-02-14 22:48:27 +0800

    push

--- init.lua
+++ init.lua
@@ -1,8 +1,15 @@
-require "core.settings"
-require "core.mappings"
-require "core.modules"
-require "core.autocommands"
-require "modules.ui.theme"
-require "custom.statusline"
-require "custom.overrides"
-require "custom.abbrev"
+require "bryant.core.settings"
+require "bryant.core.mappings"
+require "bryant.core.modules"
+require "bryant.core.autocommands"
+require "bryant.modules.ui.theme"
+require "bryant.custom.statusline"
+require "bryant.custom.overrides"
+require "bryant.custom.abbrev"
+
+--[[ vim.g.ts_highlight_lua = true
+local colorscheme_path = vim.fn.stdpath "cache" .. "/omega/highlights"
+if not vim.loop.fs_stat(colorscheme_path) then
+    require("omega.colors").compile_theme "onedark"
+end
+loadfile(colorscheme_path)() ]]
