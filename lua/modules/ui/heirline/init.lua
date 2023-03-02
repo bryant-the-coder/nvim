e5010a80e7001a5d416b9f46d94477d6ab97909d
bryant-the-coder <foobryant99@gmail.com>
2023-03-02 23:03:36 +0800

    fix heirline error

--- lua/modules/ui/heirline/init.lua
+++ lua/modules/ui/heirline/init.lua
@@ -64,65 +64,83 @@ local file_icons = {
 }
 
 local Mode = {
-    {
-        init = function(self)
-            self.mode = vim.fn.mode(1)
-        end,
+    -- get vim current mode, this information will be required by the provider
+    -- and the highlight functions, so we compute it only once per component
+    -- evaluation and store it as a component attribute
+    init = function(self)
+        self.mode = vim.fn.mode(1) -- :h mode()
 
-        static = {
-            mode_names = { -- change the strings if you like it vvvvverbose!
-                n = "NORMAL",
-                no = "NORMAL",
-                v = "VISUAL",
-                V = "VISUAL LINE",
-                [""] = "VISUAL BLOCK",
-                s = "SELECT",
-                S = "SELECT LINE",
-                [""] = "SELECT BLOCK",
-                i = "INSERT",
-                ic = "INSERT",
-                R = "REPLACE",
-                Rv = "VISUAL REPLACE",
-                c = "COMMAND",
-                cv = "VIM EX",
-                ce = "EX",
-                r = "PROMPT",
-                rm = "MOAR",
-                ["r?"] = "?",
-                ["!"] = "!",
-                t = "TERMINAL",
-            },
-            mode_colors = {
-
-                n = colors.green,
-                i = colors.nord_blue,
-                v = colors.purple,
-                V = colors.purple,
-                ["^V"] = colors.purple,
-                c = colors.teal,
-                s = vim.g.terminal_color_3,
-                S = vim.g.terminal_color_3,
-                ["^S"] = colors.yellow,
-                R = colors.yellow,
-                r = colors.yellow,
-                ["!"] = colors.light_grey,
-                t = colors.red,
-            },
+        -- execute this only once, this is required if you want the ViMode
+        -- component to be updated on operator pending mode
+        if not self.once then
+            vim.api.nvim_create_autocmd("ModeChanged", {
+                pattern = "*:*o",
+                command = "redrawstatus",
+            })
+            self.once = true
+        end
+    end,
+    -- Now we define some dictionaries to map the output of mode() to the
+    -- corresponding string and color. We can put these into `static` to compute
+    -- them at initialisation time.
+    static = {
+        mode_names = { -- change the strings if you like it vvvvverbose!
+            n = "NORMAL",
+            no = "NORMAL",
+            v = "VISUAL",
+            V = "VISUAL LINE",
+            [""] = "VISUAL BLOCK",
+            s = "SELECT",
+            S = "SELECT LINE",
+            [""] = "SELECT BLOCK",
+            i = "INSERT",
+            ic = "INSERT",
+            R = "REPLACE",
+            Rv = "VISUAL REPLACE",
+            c = "COMMAND",
+            cv = "VIM EX",
+            ce = "EX",
+            r = "PROMPT",
+            rm = "MOAR",
+            ["r?"] = "?",
+            ["!"] = "!",
+            t = "TERMINAL",
         },
-        hl = function(self)
-            local mode = self.mode:sub(1, 1) -- get only the first mode character
-            return { fg = self.mode_colors[mode] }
-        end,
-        provider = function(self)
-            -- return "%2(" .. self.mode_names[self.mode:sub(1, 1)] .. "%)" .. " "
-            return string.format(" %s ", self.mode_names[self.mode:sub(1, 1)]):upper()
-        end,
-        -- Re-evaluate the component only on ModeChanged event!
-        -- This is not required in any way, but it's there, and it's a small
-        -- performance improvement.
-        update = {
-            "ModeChanged",
+        mode_colors = {
+            n = colors.green,
+            i = colors.nord_blue,
+            v = colors.purple,
+            V = colors.purple,
+            ["^V"] = colors.purple,
+            c = colors.teal,
+            s = vim.g.terminal_color_3,
+            S = vim.g.terminal_color_3,
+            ["^S"] = colors.yellow,
+            R = colors.yellow,
+            r = colors.yellow,
+            ["!"] = colors.light_grey,
+            t = colors.red,
         },
+    },
+    -- We can now access the value of mode() that, by now, would have been
+    -- computed by `init()` and use it to index our strings dictionary.
+    -- note how `static` fields become just regular attributes once the
+    -- component is instantiated.
+    -- To be extra meticulous, we can also add some vim statusline syntax to
+    -- control the padding and make sure our string is always at least 2
+    -- characters long. Plus a nice Icon.
+    provider = function(self)
+        return "%2(" .. self.mode_names[self.mode] .. "%)"
+    end,
+    hl = function(self)
+        local mode = self.mode:sub(1, 1) -- get only the first mode character
+        return { fg = self.mode_colors[mode], bold = true }
+    end,
+    -- Re-evaluate the component only on ModeChanged event!
+    -- This is not required in any way, but it's there, and it's a small
+    -- performance improvement.
+    update = {
+        "ModeChanged",
     },
 }
 
