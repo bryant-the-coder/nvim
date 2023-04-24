7e0261c0c210d587db1107135ee94cf82ae65e0b
bryant-the-coder <foobryant99@gmail.com>
2023-04-24 09:43:45 +0800

    snippets

--- luasnippets/lua.lua
+++ luasnippets/lua.lua
@@ -27,4 +27,34 @@ return {
     parse({ trig = "nmap" }, nmap),
     parse({ trig = "imap" }, imap),
     parse({ trig = "status" }, status),
+    s(
+        {
+            trig = "if",
+            condition = function()
+                local ignored_nodes = { "string", "comment" }
+
+                local pos = vim.api.nvim_win_get_cursor(0)
+                -- Use one column to the left of the cursor to avoid a "chunk" node
+                -- type. Not sure what it is, but it seems to be at the end of lines in
+                -- some cases.
+                local row, col = pos[1] - 1, pos[2] - 1
+
+                local node_type = vim.treesitter
+                    .get_node({
+                        pos = { row, col },
+                    })
+                    :type()
+
+                return not vim.tbl_contains(ignored_nodes, node_type)
+            end,
+        },
+        fmt(
+            [[
+        if {} then
+            {}
+        end
+        ]],
+            { i(1), i(2) }
+        )
+    ),
 }
