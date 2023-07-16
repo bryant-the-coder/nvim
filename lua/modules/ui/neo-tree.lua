27393c0762b6e83ced2d77c2cd7d59190c8efc60
bryant-the-coder <foobryant99@gmail.com>
2023-07-16 12:05:53 +0800

    change neotree settings

--- lua/modules/ui/neo-tree.lua
+++ lua/modules/ui/neo-tree.lua
@@ -24,7 +24,7 @@ tree.setup({
         "Vista",
         "edgy",
     },
-    close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+    close_if_last_window = true,  -- Close Neo-tree if it is the last window left in the tab
     popup_border_style = "solid", -- "double", "none", "rounded", "shadow", "single" or "solid
     enable_git_status = true,
     enable_diagnostics = true,
@@ -67,7 +67,7 @@ tree.setup({
         git_status = {
             symbols = {
                 -- Change type
-                added = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+                added = "",    -- or "✚", but this is redundant info if you use git_status_colors on the name
                 modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
                 deleted = "✖", -- this can only be used in the git_status source
                 renamed = "", -- this can only be used in the git_status source
@@ -127,7 +127,9 @@ tree.setup({
                 --"node_modules"
             },
         },
-        follow_current_file = true, -- This will find and focus the file in the active buffer every
+        follow_current_file = {
+            enabled = true
+        },
         group_empty_dirs = true, -- when true, empty folders will be grouped together
         hijack_netrw_behavior = "open_default",
         window = {
@@ -142,7 +144,9 @@ tree.setup({
         },
     },
     buffers = {
-        follow_current_file = true, -- This will find and focus the file in the active buffer every
+        follow_current_file = {
+            enabled = true
+        },
         -- time the current file is changed while the tree is open.
         group_empty_dirs = true, -- when true, empty folders will be grouped together
         show_unloaded = true,
