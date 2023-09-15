41dba58d62349f4b0d6dbdafc5f503989d64baf0
bryant-the-coder <foobryant99@gmail.com>
2023-09-15 18:00:48 +0800

    push

--- lua/modules/notes/config.lua
+++ lua/modules/notes/config.lua
@@ -7,72 +7,68 @@ function config.neorg()
     end
 
     neorg.setup({
-        load = {
-            ["core.defaults"] = {},
-            ["core.upgrade"] = {},
-            ["core.looking-glass"] = {}, -- Enable the looking_glass module
-            ["core.itero"] = {},
-            ["core.export"] = {},
-            ["core.export.markdown"] = {
-                config = {
-                    extensions = "all",
-                },
+ load = {
+        ["core.defaults"] = {}, -- Load all the default modules
+        ["core.tempus"] = {},
+        ["core.completion"] = {
+            config = {
+                engine = "nvim-cmp",
             },
-            ["core.dirman"] = {
-                config = {
-                    workspaces = {
-                        home = "~/neorg",
-                        notes = "~/neorg/notes",
-                        task = "~/neorg/task",
-                    },
-                    open_last_workspace = false,
-                    index = "index.norg",
-                },
-            },
-            ["core.promo"] = {},
-            ["core.presenter"] = {
-                config = {
-                    zen_mode = "zen-mode",
-                    slide_count = {
-                        enable = true,
-                        position = "top",
-                        count_format = "[%d/%d]",
-                    },
-                },
-            },
-            ["core.esupports.metagen"] = {
-                config = {
-                    type = "auto",
-                },
-            },
-            ["core.completion"] = {
-                config = {
-                    engine = "nvim-cmp",
-                },
-            },
-            ["core.keybinds"] = {
-                config = {
-                    hook = function(keybinds)
-                        keybinds.remap_event("norg", "n", "tc", "core.norg.qol.todo_items.todo.task_cycle")
-                    end,
-                },
-            },
-            ["core.journal"] = {
-                config = {
-                    workspace = "home",
-                    journal_folder = "journal",
-                    use_folders = false,
-                    strategy = "flat",
-                    toc_format = "{dd, mm, yy}",
-                },
-            },
-            -- ["external.kanban"] = {},
-            -- ["core.integrations.telescope"] = {}, -- Enable the telescope module
-            ["core.highlights"] = {},
-            ["core.ui.calendar"] = {},
-            ["core.summary"] = {},
-            ["core.manoeuvre"] = {},
         },
+        ["core.upgrade"] = {},
+        ["core.looking-glass"] = {}, -- Enable the looking_glass module
+        ["core.itero"] = {},
+        ["core.export"] = {},
+        ["core.export.markdown"] = {
+            config = {
+                extensions = "all",
+            },
+        },
+
+        ["core.concealer"] = {},
+        ["core.esupports.metagen"] = {
+            config = {
+                type = "auto",
+            },
+        },
+        ["core.keybinds"] = {
+            config = {
+                default_keybinds = true,
+                neorg_leader = "\\",
+            },
+        },
+        ["core.dirman"] = { -- Manage your directories with Neorg
+            config = {
+                workspaces = {
+                    home = "~/neorg",
+                    personal = "~/neorg/personal",
+                    work = "~/neorg/work",
+                    notes = "~/neorg/notes",
+                    recipes = "~/neorg/notes/recipes",
+                },
+                index = "index.norg",
+                autodetect = true,
+                [[ -- autochdir = false, ]],
+            },
+        },
+
+        ["core.qol.toc"] = {
+            config = {
+                close_split_on_jump = true,
+                toc_split_placement = "left",
+            },
+        },
+        ["core.journal"] = {
+            config = {
+                workspace = "home",
+                journal_folder = "journal",
+                use_folders = true,
+            },
+        },
+        ["core.ui.calendar"] = {},
+        ["core.summary"] = {},
+        ["core.manoeuvre"] = {},
+    },
     })
 end
 
