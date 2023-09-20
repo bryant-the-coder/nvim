49b722f2c1652a59d387d4a39164d1ce28e06290
bryant-the-coder <foobryant99@gmail.com>
2023-09-20 20:45:27 +0800

    chore: format

--- lua/modules/notes/config.lua
+++ lua/modules/notes/config.lua
@@ -7,68 +7,67 @@ function config.neorg()
     end
 
     neorg.setup({
- load = {
-        ["core.defaults"] = {}, -- Load all the default modules
-        ["core.tempus"] = {},
-        ["core.completion"] = {
-            config = {
-                engine = "nvim-cmp",
-            },
-        },
-        ["core.upgrade"] = {},
-        ["core.looking-glass"] = {}, -- Enable the looking_glass module
-        ["core.itero"] = {},
-        ["core.export"] = {},
-        ["core.export.markdown"] = {
-            config = {
-                extensions = "all",
-            },
-        },
-
-        ["core.concealer"] = {},
-        ["core.esupports.metagen"] = {
-            config = {
-                type = "auto",
-            },
-        },
-        ["core.keybinds"] = {
-            config = {
-                default_keybinds = true,
-                neorg_leader = "\\",
-            },
-        },
-        ["core.dirman"] = { -- Manage your directories with Neorg
-            config = {
-                workspaces = {
-                    home = "~/neorg",
-                    personal = "~/neorg/personal",
-                    work = "~/neorg/work",
-                    notes = "~/neorg/notes",
-                    recipes = "~/neorg/notes/recipes",
+        load = {
+            ["core.defaults"] = {}, -- Load all the default modules
+            ["core.tempus"] = {},
+            ["core.completion"] = {
+                config = {
+                    engine = "nvim-cmp",
                 },
-                index = "index.norg",
-                autodetect = true,
-                [[ -- autochdir = false, ]],
             },
-        },
+            ["core.upgrade"] = {},
+            ["core.looking-glass"] = {}, -- Enable the looking_glass module
+            ["core.itero"] = {},
+            ["core.export"] = {},
+            ["core.export.markdown"] = {
+                config = {
+                    extensions = "all",
+                },
+            },
 
-        ["core.qol.toc"] = {
-            config = {
-                close_split_on_jump = true,
-                toc_split_placement = "left",
+            ["core.concealer"] = {},
+            ["core.esupports.metagen"] = {
+                config = {
+                    type = "auto",
+                },
             },
-        },
-        ["core.journal"] = {
-            config = {
-                workspace = "home",
-                journal_folder = "journal",
-                use_folders = true,
+            ["core.keybinds"] = {
+                config = {
+                    default_keybinds = true,
+                    neorg_leader = "\\",
+                },
             },
+            ["core.dirman"] = { -- Manage your directories with Neorg
+                config = {
+                    workspaces = {
+                        home = "~/neorg",
+                        personal = "~/neorg/personal",
+                        work = "~/neorg/work",
+                        notes = "~/neorg/notes",
+                        recipes = "~/neorg/notes/recipes",
+                    },
+                    index = "index.norg",
+                    autodetect = true,
+                    [[ -- autochdir = false, ]],
+                },
+            },
+
+            ["core.qol.toc"] = {
+                config = {
+                    close_split_on_jump = true,
+                    toc_split_placement = "left",
+                },
+            },
+            ["core.journal"] = {
+                config = {
+                    workspace = "home",
+                    journal_folder = "journal",
+                    use_folders = true,
+                },
+            },
+            ["core.summary"] = {},
+            ["core.manoeuvre"] = {},
         },
-        ["core.ui.calendar"] = {},
-        ["core.summary"] = {},
-        ["core.manoeuvre"] = {},
-    },
     })
 end
 
