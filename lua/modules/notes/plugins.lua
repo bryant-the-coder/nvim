ab6d7c5238dfa9b64b63af59e39aa3603a17f793
bryant-the-coder <foobryant99@gmail.com>
2024-03-16 17:39:52 +0800

    neorg update

--- lua/modules/notes/plugins.lua
+++ lua/modules/notes/plugins.lua
@@ -1,11 +1,28 @@
 local conf = require("modules.notes.config")
 local notes = require("utils.pack").package
 
-notes({
+--[[ notes({
     enabled = true,
     "nvim-neorg/neorg",
     lazy = false,
     run = ":Neorg sync-parsers", -- This is the important bit!
+    cmd = "Neorg",
+    ft = "norg",
+    config = conf.neorg,
+}) ]]
+
+notes({
+    "vhyrro/luarocks.nvim",
+    config = function()
+        require("luarocks").setup({})
+    end,
+})
+
+notes({
+    "nvim-neorg/neorg",
+    branch = "luarocks",
+    dependencies = { "luarocks.nvim" },
+    lazy = false,
     cmd = "Neorg",
     ft = "norg",
     config = conf.neorg,
